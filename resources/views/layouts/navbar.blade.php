c438ff1706e7d4f6279317e8d9c2001a62d7f5b6
Puteri Yudani <puteriyudani@gmail.com>
2022-11-26 01:15:13 +0700

    Tampilan Navbar dan Sidebar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,38 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SPK BLT</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="logo navbar-brand" href="#">
+                <img class="img-fluid me-2" src="img/logo.png" alt="logo" width="50" />
+                BLT-DD SMARTER
+            </a>
+            <form class="d-flex" role="login">
+                <a class="btnLogin btn btn-outline-success" href="#" role="button">LOGIN</a>
+            </form>
+        </div>
+    </nav>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
